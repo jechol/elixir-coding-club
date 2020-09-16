51de27e234640c086b3c8dbb62f72d95409dac26
Jechol Lee <mr.jechol@gmail.com>
2020-09-16 17:06:42 +0900

    remove body

--- 000_state_machine/lib/door_lock.ex
+++ 000_state_machine/lib/door_lock.ex
@@ -1,6 +1,5 @@
 defmodule DoorLock do
-  use GenStateMachine, callback_mode: [:state_functions, :state_enter]
-  import Definject
+  use GenStateMachine, callback_mode: :state_functions
 
   defmodule Data do
     defstruct code: [], input: []
@@ -20,42 +19,6 @@ defmodule DoorLock do
   end
 
   ## state callback
-  definject locked(:enter, _old_state, _data) do
-    __MODULE__.do_lock()
-    :keep_state_and_data
-  end
-
-  definject locked(:timeout, _, data) do
-    do_lock()
-    {:keep_state, %Data{data | input: []}}
-  end
-
-  definject locked(:cast, {:button, button}, %Data{code: code, input: input} = data) do
-    (input ++ [button])
-    |> Enum.reverse()
-    |> Enum.take(code |> length())
-    |> Enum.reverse()
-    |> case do
-      ^code ->
-        {:next_state, :open, %Data{data | input: []}, [{:state_timeout, 5000, :lock}]}
-
-      new_input ->
-        {:keep_state, %Data{data | input: new_input}, 5000}
-    end
-  end
-
-  definject open(:enter, _old_state, _data) do
-    __MODULE__.do_unlock()
-    :keep_state_and_data
-  end
-
-  definject open(:state_timeout, :lock, data) do
-    {:next_state, :locked, data}
-  end
-
-  definject open(:cast, {:button, _}, _) do
-    :keep_state_and_data
-  end
 
   ## actions
   def do_lock() do
