3dce04fb5b184fef5d0193a367a51ca5b26edc96
Jechol Lee <mr.jechol@gmail.com>
2020-09-22 11:13:31 +0900

    rename

--- 010_collections/mix.exs
+++ 010_collections/mix.exs
@@ -3,7 +3,7 @@ defmodule MyEnum.MixProject do
 
   def project do
     [
-      app: :my_enum,
+      app: :collections,
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
