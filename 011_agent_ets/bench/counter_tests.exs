e1e0103bb992b34b79d14188ce61fa0db1466b50
Jechol Lee <mr.jechol@gmail.com>
2020-09-24 13:13:18 +0900

    Fix write bench

--- 011_agent_ets/bench/counter_tests.exs
+++ 011_agent_ets/bench/counter_tests.exs
@@ -1,16 +1,19 @@
 bench_counter = fn counter_mod ->
   max = 100_000
   buckets = 1000
+  watermark = max |> div(buckets)
 
   {:ok, counter} = counter_mod.start_link()
 
-  1..max
-  |> Task.async_stream(fn c -> counter_mod.increment(counter, rem(c, buckets)) end)
+  0..(buckets - 1)
+  |> Task.async_stream(fn b ->
+    0..(watermark - 1) |> Enum.each(fn _ -> counter_mod.increment(counter, b) end)
+  end)
   |> Stream.run()
 
   sum =
     0..(buckets - 1)
-    |> Task.async_stream(fn c -> counter_mod.value(counter, rem(c, buckets)) end)
+    |> Task.async_stream(fn b -> counter_mod.value(counter, b) end)
     |> Enum.reduce(0, fn {:ok, n}, acc -> acc + n end)
 
   if sum != max do
--- 011_agent_ets/lib/agent_counter.ex
+++ 011_agent_ets/lib/agent_counter.ex
@@ -2,11 +2,14 @@ defmodule AgentCounter do
   @behaviour Counter
 
   def start_link() do
+    Agent.start_link(fn -> %{} end)
   end
 
   def value(pid, key) do
+    Agent.get(pid, &Map.get(&1, key, 0))
   end
 
   def increment(pid, key) do
+    Agent.update(pid, fn map -> Map.update(map, key, 1, &(&1 + 1)) end)
   end
 end
