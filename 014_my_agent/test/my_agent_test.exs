8c9ba16aa3f3d977d54b543f5a22a8ced46b4ac6
Jechol Lee <mr.jechol@gmail.com>
2020-09-26 22:17:10 +0900

    my agent

--- 014_my_agent/test/my_agent_test.exs
+++ 014_my_agent/test/my_agent_test.exs
@@ -2,7 +2,19 @@ defmodule MyAgentTest do
   use ExUnit.Case
   doctest MyAgent
 
-  test "greets the world" do
-    assert MyAgent.hello() == :world
+  test "Agent for reference" do
+    {:ok, pid} = Agent.start_link(fn -> %{} end)
+    assert Agent.get(pid, fn x -> x end) == %{}
+
+    Agent.update(pid, fn map -> Map.put(map, :foo, :bar) end)
+    assert Agent.get(pid, fn map -> Map.get(map, :foo) end) == :bar
+  end
+
+  test "MyAgent for reference" do
+    {:ok, pid} = MyAgent.start_link(fn -> %{} end)
+    assert MyAgent.get(pid, fn x -> x end) == %{}
+
+    MyAgent.update(pid, fn map -> Map.put(map, :foo, :bar) end)
+    assert MyAgent.get(pid, fn map -> Map.get(map, :foo) end) == :bar
   end
 end
