b9d342a984e714e848a9c4865379d33b967889ff
Jechol Lee <mr.jechol@gmail.com>
2020-09-28 09:03:28 +0900

    done my agent

--- 014_my_agent/lib/my_agent.ex
+++ 014_my_agent/lib/my_agent.ex
@@ -1,18 +1,29 @@
 defmodule MyAgent do
-  @moduledoc """
-  Documentation for `MyAgent`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  def start_link(init_fun) do
+    GenServer.start_link(__MODULE__, init_fun)
+  end
 
-  ## Examples
+  def init(init_fun) do
+    {:ok, init_fun.()}
+  end
 
-      iex> MyAgent.hello()
-      :world
+  def get(pid, get_fun) do
+    GenServer.call(pid, {:get, get_fun})
+  end
 
-  """
-  def hello do
-    :world
+  def update(pid, update_fun) do
+    GenServer.call(pid, {:update, update_fun})
+  end
+
+  # callback
+
+  def handle_call({:get, get_fun}, _from, state) do
+    {:reply, get_fun.(state), state}
+  end
+
+  def handle_call({:update, update_fun}, _from, state) do
+    {:reply, :ok, update_fun.(state)}
   end
 end
--- 014_my_agent/test/my_agent_test.exs
+++ 014_my_agent/test/my_agent_test.exs
@@ -1,20 +1,15 @@
 defmodule MyAgentTest do
   use ExUnit.Case
-  doctest MyAgent
 
-  test "Agent for reference" do
-    {:ok, pid} = Agent.start_link(fn -> %{} end)
-    assert Agent.get(pid, fn x -> x end) == %{}
+  for agent <- [Agent, MyAgent] do
+    @agent agent
 
-    Agent.update(pid, fn map -> Map.put(map, :foo, :bar) end)
-    assert Agent.get(pid, fn map -> Map.get(map, :foo) end) == :bar
-  end
+    test "#{@agent}" do
+      {:ok, pid} = @agent.start_link(fn -> %{} end)
+      assert @agent.get(pid, fn x -> map_size(x) end) == 0
 
-  test "MyAgent for reference" do
-    {:ok, pid} = MyAgent.start_link(fn -> %{} end)
-    assert MyAgent.get(pid, fn x -> x end) == %{}
-
-    MyAgent.update(pid, fn map -> Map.put(map, :foo, :bar) end)
-    assert MyAgent.get(pid, fn map -> Map.get(map, :foo) end) == :bar
+      :ok = @agent.update(pid, fn map -> Map.put(map, :foo, :bar) end)
+      assert @agent.get(pid, fn map -> Map.get(map, :foo) end) == :bar
+    end
   end
 end
