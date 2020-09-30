88d063db19b9dc275e822896e59ee834a90bb303
Jechol Lee <mr.jechol@gmail.com>
2020-09-30 11:50:55 +0900

    refactor write

--- 011_agent_ets/bench/cache_write_bench.exs
+++ 011_agent_ets/bench/cache_write_bench.exs
@@ -1,3 +1,6 @@
+schedulers = :erlang.system_info(:schedulers)
+repeat = 10000
+
 bench_write = fn cache_mod, input ->
   {:ok, cache} = cache_mod.start_link()
 
@@ -9,42 +12,27 @@ bench_write = fn cache_mod, input ->
   |> Stream.run()
 end
 
-schedulers = :erlang.system_info(:schedulers)
-
 Benchee.run(
-  %{
-    "AgentCache.Write" => fn input ->
-      bench_write.(AgentCache, input)
-    end,
-    "EtsCache.Write" => fn input ->
-      bench_write.(EtsCache, input)
-    end
-  },
+  [AgentCache, EtsCache]
+  |> Enum.map(fn cache_mod ->
+    {"#{cache_mod}", fn input -> bench_write.(cache_mod, input) end}
+  end),
   inputs:
     [{1, 0.1}, {1, 0.5}, {schedulers, 0.1}, {schedulers, 0.5}]
     |> Enum.map(fn setup = {tasks, conflict} ->
       {"(tasks: #{tasks}, conflict: #{conflict})", setup}
     end),
   before_scenario: fn {tasks, conflict} ->
-    repeat = 10000
+    import StreamData
+
     task_repeat = repeat |> div(tasks)
     keys = ((repeat - 1) * (1 - conflict)) |> trunc() |> Kernel.+(1)
 
-    input =
-      (fn ->
-         import StreamData
-
-         1..tasks
-         |> Enum.map(fn _ ->
-           {integer(1..keys), term() |> resize(10)}
-           |> tuple()
-           |> Enum.take(task_repeat)
-         end)
-       end).()
-
-    ^tasks = input |> Enum.count()
-    ^repeat = input |> Enum.map(&Enum.count/1) |> Enum.sum()
-
-    input
+    1..tasks
+    |> Enum.map(fn _ ->
+      {integer(1..keys), term() |> resize(10)}
+      |> tuple()
+      |> Enum.take(task_repeat)
+    end)
   end
 )
