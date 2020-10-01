1ec71d5d2b55e2a3432e9eda4cc84edd75f367d3
Jechol Lee <mr.jechol@gmail.com>
2020-10-02 08:30:47 +0900

    mix new learn_clustering

---
+++ 020_learn_clustering/lib/clustering.ex
@@ -0,0 +1,18 @@
+defmodule Clustering do
+  @moduledoc """
+  Documentation for `Clustering`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Clustering.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 020_learn_clustering/test/clustering_test.exs
@@ -0,0 +1,8 @@
+defmodule ClusteringTest do
+  use ExUnit.Case
+  doctest Clustering
+
+  test "greets the world" do
+    assert Clustering.hello() == :world
+  end
+end
