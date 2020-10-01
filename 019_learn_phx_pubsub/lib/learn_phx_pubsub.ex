280ea7ae5d81e57a6359be84c5d853382a5b0004
Jechol Lee <mr.jechol@gmail.com>
2020-10-01 22:58:34 +0900

    Add learn_phx_pubsub PubSub

--- 019_learn_phx_pubsub/lib/learn_phx_pubsub.ex
+++ 019_learn_phx_pubsub/lib/learn_phx_pubsub.ex
@@ -1,4 +1,15 @@
 defmodule LearnPhxPubsub do
+  use Application
+  alias Phoenix.PubSub
+
+  def start(_type, _args) do
+    children = [
+      {PubSub, name: :learn_phx_pubsub}
+    ]
+
+    Supervisor.start_link(children, strategy: :rest_for_one)
+  end
+
   @moduledoc """
   Documentation for `LearnPhxPubsub`.
   """
--- 019_learn_phx_pubsub/mix.exs
+++ 019_learn_phx_pubsub/mix.exs
@@ -14,13 +14,15 @@ defmodule LearnPhxPubsub.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {LearnPhxPubsub, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:phoenix_pubsub, "~> 2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
