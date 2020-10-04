3a952019b770bacd3c4848bf4ec0406b5733cf32
Jechol Lee <mr.jechol@gmail.com>
2020-10-05 06:47:32 +0900

    wip

--- 020_learn_clustering/lib/clustering.ex
+++ 020_learn_clustering/lib/clustering.ex
@@ -1,4 +1,25 @@
 defmodule Clustering do
+  use Application
+
+  def start(_type, _args) do
+    setup_clustering()
+
+    Supervisor.start_link([], strategy: :one_for_one, name: __MODULE__)
+  end
+
+  def setup_clustering do
+    # Turn node into a distributed node with the given name
+    :net_kernel.start([:"master@127.0.0.1", :longnames])
+    # Allow spawned nodes to fetch all code from this node
+    :erl_boot_server.start([])
+    :erl_boot_server.add_slave({127, 0, 0, 1})
+
+    for i <- [0, 1] do
+      :slave.start('127.0.0.1', :"slave#{i}", '-loader inet -hosts 127.0.0.1')
+      :rpc.block_call()
+    end
+  end
+
   @moduledoc """
   Documentation for `Clustering`.
   """
