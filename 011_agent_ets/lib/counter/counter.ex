887f0e2285faa33c1a401844d1fe1db8fdd0a8fa
Jechol Lee <mr.jechol@gmail.com>
2020-10-08 13:01:01 +0900

    Remove counter example from agent_ets

--- 011_agent_ets/lib/counter/counter.ex
+++
@@ -1,7 +0,0 @@
-defmodule Counter do
-  @type counter :: reference() | pid()
-
-  @callback start_link() :: {:ok, counter()}
-  @callback value(counter(), any()) :: integer()
-  @callback increment(counter(), any()) :: integer()
-end
