5e23ec096c8aaa295b7f71cf0c39d3ae63d8d4c1
Jechol Lee <mr.jechol@gmail.com>
2020-10-08 23:26:27 +0900

    refactor my_registry_test

--- 10_process/15_my_registry/test/my_registry_test.exs
+++ 10_process/15_my_registry/test/my_registry_test.exs
@@ -5,16 +5,17 @@ defmodule MyRegistryTest do
     @registry registry
 
     test "#{@registry} register/lookup/unregister" do
+      reg_name = [@registry, Test1Reg] |> Module.concat()
       this = self()
 
-      {:ok, _} = @registry.start_link(keys: :unique, name: Test1Reg)
-      {:ok, _} = @registry.register(Test1Reg, "hello", "world")
+      {:ok, _} = @registry.start_link(keys: :unique, name: reg_name)
+      {:ok, _} = @registry.register(reg_name, "hello", "world")
 
-      {:error, {:already_registered, ^this}} = @registry.register(Test1Reg, "hello", "world")
+      {:error, {:already_registered, ^this}} = @registry.register(reg_name, "hello", "world")
 
-      [{^this, "world"}] = @registry.lookup(Test1Reg, "hello")
-
-      :ok = @registry.unregister(Test1Reg, "hello")
+      [{^this, "world"}] = @registry.lookup(reg_name, "hello")
+      :ok = @registry.unregister(reg_name, "hello")
+      [] = @registry.lookup(reg_name, "hello")
     end
 
     test "{:via, #{@registry}, {registry, key}}" do
@@ -23,7 +24,6 @@ defmodule MyRegistryTest do
 
       name = {:via, @registry, {reg_name, "agent"}}
       {:ok, _} = Agent.start_link(fn -> 0 end, name: name)
-      name |> IO.inspect()
       0 = Agent.get(name, & &1)
     end
   end
