e80555bbcdaf79f27285caaf928c18a5d192897d
Jechol Lee <mr.jechol@gmail.com>
2020-10-15 17:37:49 +0900

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -4,17 +4,12 @@ let
     sha256 = "0dxrfr0w5ksvpjwz0d2hy7x7dirnc2xk9nw1np3wr6kvdlzhs3ik";
   }) { };
   jechol = import (fetchTarball {
-    url = "https://github.com/jechol/nur-packages/archive/v2.0.tar.gz";
-    sha256 = "00ii854c5srffq7zn5bi9m1gdyvqcxjs1pryw32ybcl2daw3d0k5";
+    url = "https://github.com/jechol/nix-beam/archive/v3.0.tar.gz";
+    sha256 = "17z11hxc9z9ph0p5im9wd635g2ha9fm9k8vkz7rjpcqzsj3iwz7m";
   }) { };
 in nixpkgs.mkShell {
   buildInputs = [
-    jechol.beam.main.erlangs.erlang_22_0
-    jechol.beam.main.packages.erlang_22_0.elixirs.elixir_1_10_0
-    nixpkgs.nodejs-14_x
-    nixpkgs.ruby_2_7
-    nixpkgs.rubyPackages_2_7.rubocop
-    nixpkgs.rubyPackages_2_7.pry
-    nixpkgs.python39
+    jechol.beam.main.erlangs.erlang_23_1
+    jechol.beam.main.packages.erlang_23_1.elixirs.elixir_1_11_0
   ];
 }
