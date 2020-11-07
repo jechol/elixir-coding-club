efe76827c2a33f37960fd223796b506242b15287
Jechol Lee <mr.jechol@gmail.com>
2020-11-07 17:03:47 +0900

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,15 @@
 let
   nixpkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/20.09-alpha.tar.gz";
-    sha256 = "0dxrfr0w5ksvpjwz0d2hy7x7dirnc2xk9nw1np3wr6kvdlzhs3ik";
+    url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
+    sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
   }) { };
-  jechol = import (fetchTarball {
-    url = "https://github.com/jechol/nix-beam/archive/v3.0.tar.gz";
-    sha256 = "17z11hxc9z9ph0p5im9wd635g2ha9fm9k8vkz7rjpcqzsj3iwz7m";
+  beam = import (fetchTarball {
+    url = "https://github.com/jechol/nix-beam/archive/v4.3.tar.gz";
+    sha256 = "117c43s256i2nzp0zps9n2f630gm00yhsbgc78r2qimi0scdxf52";
   }) { };
 in nixpkgs.mkShell {
   buildInputs = [
-    jechol.beam.main.erlangs.erlang_23_1
-    jechol.beam.main.packages.erlang_23_1.elixirs.elixir_1_11_0
+    beam.erlang.v23_1
+    beam.pkg.v23_1.elixir.v1_11_0
   ];
 }
